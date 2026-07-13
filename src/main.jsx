18956ecaffbc39b943d0056e1a06c9b7015e2505
giovannimazzi <gioelor@libero.it>
2026-07-13 02:28:59 +0200

    fix navbar collapse bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import * as bootstrap from "bootstrap";
+
 // Foglio di stile custom
 import "./assets/css/index.css";
 
